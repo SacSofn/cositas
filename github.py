37307fc213386feb357fa3a3eb0ce7bc506a34af
SacSofn <sacullol.contreras@gmail.com>
2024-06-19 19:15:07 -0400

    Update github.py

--- github.py
+++ github.py
@@ -1,2 +1,9 @@
-print("hola","github")
+print("hola","github",sep=" o.O ")
 nombre = input("ingrese su nombre para continuar")
+while not nombre.isalpha():
+  print("ingrese un nombre valido")
+  nombre = input("ingrese su nombre para continuar")
+
+print("ahora su nombre sera puesto alrevez")
+for x in reversed(nombre):
+  print(x)
